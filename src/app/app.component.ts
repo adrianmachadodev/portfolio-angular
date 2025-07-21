ec7379afc570f34c695fdf73ea60c56b0f137206
Adrian Machado <machadoadriandev@gmail.com>
2025-07-21 19:57:45 -0300

    logo loading and sidebar info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'portfolio-angular-app';
   config: any;
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    const appRoot = document.querySelector('app-root') as HTMLElement;
+
+    const splash = document.getElementById('splash-img');
+    setTimeout(() => {
+      if(splash) splash.remove();
+
+      if(appRoot) appRoot.style.display = 'block';
+
+
+      if (splash) {
+        splash.style.display = 'none';
+      }
+    }, 1500);
+  }
 }
